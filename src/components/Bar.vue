d7879689165a748602b7a09a05a2634be4024bba
lee <2522304924@qq.com>
2020-11-03 20:50:23 +0800

    兄弟组件数据独立

---
+++ src/components/Bar.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h2>Bar 组件</h2>
+        <p>{{ count }}</p>
+        <button @click="increment">自增</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                count: 0
+            }
+        },
+        methods: {
+            increment() {
+                this.count++;
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
